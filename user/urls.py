c729aeed7bd4b5200022af9fc0aa4460885b1f08
perez1187 <o.perez1187@gmail.com>
2022-08-31 18:35:40 +0200

    logout

--- user/urls.py
+++ user/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.RegisterApi.as_view(), name="Register"),
     path('login/', views.LoginApi.as_view(), name="Login"),
     path('me/', views.UserApi.as_view(), name="me"),
+    path('logout/', views.LogoutApi.as_view(), name="logout"),
 ]
--- user/views.py
+++ user/views.py
@@ -55,3 +55,14 @@ class UserApi(views.APIView):
         serializer=user_serializer.UserSerializer(user)
 
         return response.Response(serializer.data)
+
+class LogoutApi(views.APIView):
+    authentication_classes= (authentication.CustomUserAuthentication, )
+    permission_classes=(permissions.IsAuthenticated, )
+
+    def post(self, request):
+        resp = response.Response()
+        resp.delete_cookie("jwt")
+        resp.data = {"message": "logout ;) "}
+
+        return resp
