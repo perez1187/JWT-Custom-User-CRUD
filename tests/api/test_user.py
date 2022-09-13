023ffead68251e6fa94337ca253106a88e2bb5a4
perez1187 <o.perez1187@gmail.com>
2022-09-13 12:00:59 +0200

    next tests

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,11 +1,16 @@
-import imp
 import pytest
 from rest_framework.test import APIClient
 
-client = APIClient()
+# client = APIClient()
+'''
+    we can create client in conftest.py and then we paste client here:
+    def test_register_user(client):
+    def test_login_user(user, client):
+    etc
+'''
 
 @pytest.mark.django_db # we need this decorator when we writing to db
-def test_register_user():
+def test_register_user(client): # remember to name def test_ (from pytest.ini)
     payload = dict(
         first_name="Harry",
         last_name="Potter", 
@@ -23,3 +28,34 @@ def test_register_user():
     assert data["email"] == payload["email"]
     assert "id" in data
     assert "password" not in data
+
+
+@pytest.mark.django_db
+def test_login_user(user, client):
+    '''
+        we can create credentials manualy or use function from conftest.py
+        then we paste user here: def test_login_user(user)
+    '''
+    
+    # payload = dict(
+    #     first_name="Harry",
+    #     last_name="Potter", 
+    #     email="harry@hooogwards.com",
+    #     password="timeforsometesting1"
+    # )
+
+    # client.post("/api/register/", payload)
+
+    response = client.post("/api/login/", dict(email="harry@hooogwards.com",password="timeforsometesting1"))
+
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_login_user_fail(client):
+    response = client.post("/api/login/", dict(email="harry@hooogwards.com",password="timeforsometesting1"))
+
+    assert response.status_code == 403
+
+@pytest.mark.django_db
+def test_get_me():
+    pass
---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from rest_framework.test import APIClient
+from user import services as user_services
+
+# we can do it with user model as well
+@pytest.fixture
+def user():
+    user_dc = user_services.UserDataClass( # user data class
+        first_name="Harry",
+        last_name="Potter", 
+        email="harry@hooogwards.com",
+        password="timeforsometesting1"
+    )
+    user = user_services.create_user(user_dc=user_dc)
+
+    return user
+
+@pytest.fixture
+def client():
+    return APIClient()
